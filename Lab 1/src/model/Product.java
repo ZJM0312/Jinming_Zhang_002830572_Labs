4db137ccd6d0a4eff8e7391252f75b640423a9f8
ZJM0312 <zhang.jinmin@northeastern.edu>
2023-09-11 20:41:05 -0400

    Lab1

---
+++ Lab 1/src/model/Product.java
@@ -0,0 +1,60 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author zhangjinming
+ */
+public class Product{
+    private String name;
+    private String descr;
+    private String availnum;
+    private String price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getAvailnum() {
+        return availnum;
+    }
+
+    public void setAvailnum(String availnum) {
+        this.availnum = availnum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setAvailNum(String text) {
+
+    }
+
+    public String getAvailNum() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
